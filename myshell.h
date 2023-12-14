1b4d10787e395c59b8aa9aea4b48d30f3e3949e0
wawi <wawigladys@gmail.com>
2023-12-13 17:45:11 -0800

    Create header file

---
+++ myshell.h
@@ -0,0 +1,13 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+#endif
