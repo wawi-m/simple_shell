34f7e91de1aa5a4f375a1ce426490b93e4a4e476
wawi-m <122799427+wawi-m@users.noreply.github.com>
2023-12-14 05:33:04 +0300

    Update myshell.h

--- myshell.h
+++ myshell.h
@@ -9,5 +9,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+int main(void);
+int main(int ac, char **av);
+int main(int ac, char **av, char **env);
+char *_getenv(const char *name);
+int _setenv(const char *name, const char *value, int overwrite);
+int _unsetenv(const char *name);
 
 #endif
